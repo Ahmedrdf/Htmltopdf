0db1a640e979c4b0bb8a2e24483340513e595ba9
ahmedLf <ahmedrdf.mansour@gmail.com>
2020-07-21 21:46:12 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
-
+import { Component,ViewChild,ElementRef } from '@angular/core';
+import * as jsPDF from 'jspdf';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('content') content : ElementRef ;
+
+  public downloadPDF(){
+    let doc = new jsPDF();
+    let specialElementHandlers = {
+      '#editor': function(element , renderer){
+        return true ; 
+      }
+    };
+    let content = this.content.nativeElement;
+    doc.fromHTML(content.innerHTML,15,15,{
+
+      'width': 190,
+      'elmentHandler':specialElementHandlers
+    });
+
+doc.save('test.pdf');
+  }
   title = 'htmltopdf';
 }
